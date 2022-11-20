db87bba0379cf04bc37ed186c928b548e6dab096
preet-tibrewala <tibrewap@oregonstate.edu>
2022-11-20 14:36:09 -0800

    Increased efficiency

--- Sources/CrackStation/CrackStation.swift
+++ Sources/CrackStation/CrackStation.swift
@@ -2,43 +2,24 @@ import Foundation
 
 public protocol Decrypter {
     init()
-
-    /// Either returns the cracked plain-text password
-    /// or, if unable to crack, then returns nil.
-    /// - Parameter shaHash: The SHA-1 or SHA-256 digest that corresponds to the encrypted password.
-    /// - Returns: The underlying plain-text password if `shaHash` was successfully cracked. Otherwise returns nil.
     func decrypt(shaHash: String) -> String?
 }
 public struct CrackStation: Decrypter {
+
+    var lookupTable: Dictionary <String,String> = [:]
     public init() {
-    }
-
-    func loadDictionaryFromDisk() throws -> [String : String] {
-        guard let path = Bundle.module.url(forResource: "data", withExtension: "json") else { return [:] }
-
-            let IPdata = try Data(contentsOf: path)
-            let OPjson = try JSONSerialization.jsonObject(with: IPdata)
-
-            if let datatable: Dictionary = OPjson as? Dictionary<String, String> {
-                return datatable
-            } else {
-                return [:]
+        do{
+            guard let path = Bundle.module.url(forResource: "data", withExtension: "json") else {return}
+            let data = try Data(contentsOf: path)
+            let jsonoutput = try JSONSerialization.jsonObject(with: data) as? Dictionary<String, String>
+            lookupTable = jsonoutput ?? [:]
+        } catch {
+                print("Error Loading from dictionary")
         }
     }
     
     public func decrypt(shaHash: String) -> String? {
-        do{
-                let datatable = try loadDictionaryFromDisk()
-                let ans = datatable[shaHash] ?? nil
-                
-                return ans
-            } catch {
-                
-                return nil
-            }
-        } 
+        let password = lookupTable[shaHash]
+        return password
+    }  
 }
-
-
-
-
